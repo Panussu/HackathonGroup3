44d1ba635e3d044990ea5e3e6ccfaa9a20bd9d73
Thara Nga-sa-nga <tharangasanga@gmail.com>
2026-06-10 22:56:54 +0700

    Update and rename trainmodel.py to trainmodel.ipynb
    Signed-off-by: Thara Nga-sa-nga <tharangasanga@gmail.com>

--- trainmodel.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('Export_Data.csv')
-
-print("=== โครงสร้างข้อมูลเบื้องต้น ===")
-print(df.info())
-
-df['Time'] = pd.to_datetime(df['Time'])
-df = df.sort_values('Time').reset_index(drop=True)
